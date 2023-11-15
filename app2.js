d2069c8be7a2e578a722470eed48da76b2bbef14
AlexsanderUlrich <alexsanderulrich@gmail.com>
2023-11-15 00:46:38 -0300

    Ajustes

--- app2.js
+++ app2.js
@@ -1,42 +1,59 @@
-//let titulo = document.querySelector("h1");
-//titulo.innerHTML = "Jogo do número secreto";
-
 let numeroSecreto = getNumeroAleatorio();
-
+let quantidaDeTentativas = 1;
 
 
 function getNumeroAleatorio() {
     return parseInt(Math.random() *100 +1);    
 }
-console.log(numeroSecreto)
-
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-exibirTextoNaTela("h1", "jogo do número secreto");
-exibirTextoNaTela("p", "Digite um número de 1 a 100");
 
+function mensagemInicial () {
+    exibirTextoNaTela("h1", "jogo do número secreto");
+    exibirTextoNaTela("p", "Digite um número de 1 a 100");
+}
 
-
+mensagemInicial ();
 
 
 function verificarChute() { 
     let chute = document.querySelector("input").value;
+    
 
     if (chute == numeroSecreto) {
+        let palavraTentativa = quantidaDeTentativas > 1 ? 'tentativa' : 'tentativas';
+        let mensagem = `Você acertou com ${quantidaDeTentativas} ${palavraTentativa}`;
         exibirTextoNaTela ("h1", "você acertou o número secreto");
-        exibirTextoNaTela ("p", "Só tenho duas palavras... PARA BÉNS!!!");
-    }    
-    else { 
-        if (chute < numeroSecreto) {
+        exibirTextoNaTela ("p", mensagem);        
+        habilitarBotao()
+    }
+        else { 
+            if (chute < numeroSecreto) {
             exibirTextoNaTela ("p", `O número secreto é maior que ${chute}`);
-        }
-            else {
-                exibirTextoNaTela ("p", `O número secreto é menor que ${chute}`);
             }
+                else {
+                exibirTextoNaTela ("p", `O número secreto é menor que ${chute}`);
+                }
+
+                    quantidaDeTentativas++;
+                    limparCampo();
     }
 }
 
+function limparCampo () {
+    chute = document.querySelector("input");        
+    chute.value = "";
+}
 
+function habilitarBotao () {
+    document.getElementById("reiniciar").removeAttribute ("disabled");
+}
+
+function novoJogo () {
+    limparCampo ();
+    mensagemInicial ();
+    document.getElementById("reiniciar").setAttribute ("disabled", true);
+}
